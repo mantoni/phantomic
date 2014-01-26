76b5bb4ad87549158820140080f440fea3d3f3e0
Maximilian Antoni <mail@maxantoni.de>
2014-01-26 21:18:55 +0100

    Fixed test cases

--- test/suite.sh
+++ test/suite.sh
@@ -1,26 +1,26 @@
 . assert.sh/assert.sh
 
-assert "node . < test/hello.js" "hello phantom.js"
+assert "node bin/cmd.js < test/hello.js" "hello phantom.js"
 
-assert "node . < test/async.js" "Oh, hi!"
+assert "node bin/cmd.js < test/async.js" "Oh, hi!"
 
-assert_raises "node . < test/hello.js"
+assert_raises "node bin/cmd.js < test/hello.js"
 
-assert_raises "node . < test/logerror.js" 1
+assert_raises "node bin/cmd.js < test/logerror.js" 1
 
-assert_raises "node . < test/error.js" 1
+assert_raises "node bin/cmd.js < test/error.js" 1
 
-assert "node . < test/logerror.js" "Whoups!"
+assert "node bin/cmd.js < test/logerror.js" "Whoups!"
 
-assert "node . < test/logerrors.js" "1
+assert "node bin/cmd.js < test/logerrors.js" "1
 2
 3"
 
-assert "node . < test/error.js" "Error: Ouch!
+assert "node bin/cmd.js < test/error.js" "Error: Ouch!
     at throws #2
     at a #5
     at b #8"
 
-assert "browserify test/browserify.js | node ." "hello emitter"
+assert "browserify test/browserify.js | node bin/cmd.js" "hello emitter"
 
 assert_end
