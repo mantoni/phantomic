e14b3e4939e1f23732cbe5fa3c87493cadebeb25
Maximilian Antoni <mail@maxantoni.de>
2014-03-22 23:54:17 +0100

    Experimental debugger support

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,19 @@
 
 var phantomic = require('../lib/phantomic.js');
 
-phantomic(process.stdin, function (code) {
+var args = process.argv.slice(2);
+var debug = false;
+var input = process.stdin;
+
+if (args[0] === '--debug') {
+  debug = true;
+  args.shift();
+}
+if (args.length) {
+  console.log(args[0]);
+  input = require('fs').createReadStream(args[0]);
+}
+
+phantomic(input, { debug : debug }, function (code) {
   process.exit(code);
 }).pipe(process.stdout);
