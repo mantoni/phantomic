c5e2ac2c068b92d6162a92042d4c0b9277a3b2da
Phred <fearphage@gmail.com>
2016-08-22 12:36:38 -0500

    attempted to make tests pass in any time zone
    * added test section labels

--- test/suite.sh
+++ test/suite.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 . assert.sh/assert.sh
 
@@ -13,6 +13,7 @@ assert "node bin/cmd.js < test/async-long.js" "1
 3
 4
 5"
+assert_end basic
 
 assert_raises "echo '' | node bin/cmd.js --brout" 1
 
@@ -28,6 +29,8 @@ assert "node bin/cmd.js < test/logerrors.js" "1
 2
 3"
 
+assert_end error
+
 assert "node bin/cmd.js --port 42000 < test/error.js | head -n 2" "Error: Ouch!
     at http://localhost:42000/js/bundle:2"
 
@@ -37,8 +40,10 @@ assert "node_modules/.bin/browserify --debug test/sourcemaps-console.js | node b
 assert "node_modules/.bin/browserify --debug test/sourcemaps-uncaught.js | node bin/cmd.js | head -n 2" "Error: oups
       at test/sourcemaps-uncaught.js:2"
 
-PHANTOM=`which phantomjs`
-NODE=`which node`
+assert_end debugging
+
+PHANTOM=$(which phantomjs)
+NODE=$(which node)
 assert "PATH=; $NODE bin/cmd.js < test/hello.js" "Cannot find phantomjs. Make sure it's in your \$PATH, or specify with --phantomjs."
 # Don't know why this fails. Running this command from the command line works as expected
 #assert "PATH=; $NODE bin/cmd.js --phantomjs $PHANTOM < test/hello.js" "hello phantom.js"
@@ -51,19 +56,16 @@ assert_raises "node bin/cmd.js < test/ignore-ssl-errors.js" 1
 
 assert "node bin/cmd.js --web-security false --ignore-ssl-errors true < test/ignore-ssl-errors.js" "--ignore-ssl-errors=true"
 
+assert_end "cli flag"
+
 assert "node bin/cmd.js < test/navigation.js" "no navigation"
 
+utc=$(date +%s --utc -d "12:00:00")
 export TZ='Europe/Berlin'
-DST=`date +%Z`
-if [[ $DST == CEST ]]; then
-  OFFSET_EU="-120"
-  OFFSET_US="240"
-else
-  OFFSET_EU="-60"
-  OFFSET_US="300"
-fi
-assert "node bin/cmd.js < test/timezone.js" $OFFSET_EU
+offset=$((($(date +%s --utc -d "12:00:00 $(date +%Z)") - $utc) / 60))
+assert "node bin/cmd.js < test/timezone.js" $offset
 export TZ='America/New_York'
-assert "node bin/cmd.js < test/timezone.js" $OFFSET_US
+offset=$((($(date +%s --utc -d "12:00:00 $(date +%Z)") - $utc) / 60))
+assert "node bin/cmd.js < test/timezone.js" $offset
 
-assert_end
+assert_end miscellaneous
